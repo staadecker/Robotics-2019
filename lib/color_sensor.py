ff3aa9458db8ca87696b769562e2cd80983c586c
Martin Staadecker <machstg@gmail.com>
2019-03-17 22:11:26 -0400

    Add color sensor class
    Signed-off-by: Martin Staadecker <machstg@gmail.com>

---
+++ lib/color_sensor.py
@@ -0,0 +1,26 @@
+import abc
+from ev3dev2.sensor.lego import ColorSensor
+
+
+class BaseColorSensor(abc.ABC):
+    """Interface for a color sensor"""
+
+    @abc.abstractmethod
+    def get_color(self):
+        """
+        Method that gets the color under the sensor
+
+        Returns:
+            A number representing the color
+        """
+        pass
+
+
+# TODO : Test
+class ColorSensor(BaseColorSensor):
+    def __init__(self, port):
+        self.color_sensor = ColorSensor(port)
+        self.color_sensor.mode = ColorSensor.MODE_COL_COLOR
+
+    def get_color(self):
+        return self.color_sensor.value(1)

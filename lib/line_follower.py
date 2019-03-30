10ee7ca19494f32b823fceb338d7e1eafbfcce3f
Martin Staadecker <machstg@gmail.com>
2019-03-29 21:31:08 -0400

    Write basic untested line follower
    Signed-off-by: Martin Staadecker <machstg@gmail.com>

--- lib/line_follower.py
+++ lib/line_follower.py
@@ -1,13 +1,21 @@
 from movement_controller import MovementController
+from color_sensor import ColorSensorWrapper
+
+WHITE_REFLECTION = 1
+BLACK_REFLECTION = 0
+PERCENTAGE_OF_MIDDLE = 0.5
+KP_FACTOR = 1
 
 
 class LineFollower:
     """Responsible for following a line using color sensors"""
 
-    def __init__(self, movement_controller, left_color_sensor, right_color_sensor):
+    def __init__(self, movement_controller: MovementController, left_color_sensor: ColorSensorWrapper,
+                 right_color_sensor: ColorSensorWrapper):
         self.movement_controller = movement_controller
         self.left_color_sensor = left_color_sensor
         self.right_color_sensor = right_color_sensor
+        self.middle_value = (WHITE_REFLECTION - BLACK_REFLECTION) * PERCENTAGE_OF_MIDDLE + BLACK_REFLECTION
 
     def follow_line(self, should_stop_following, use_left_sensor=True):
         """
@@ -16,4 +24,11 @@ class LineFollower:
             should_stop_following : Method returning true or false depending on whether the line follower should stop
             use_left_sensor : Determines whether it should follow the line with the left color sensor or the right one
         """
-        pass  # TODO
+
+        while not should_stop_following():
+            if use_left_sensor:
+                correction = KP_FACTOR * (self.middle_value - self.left_color_sensor.get_reflected())
+                self.movement_controller.steer(correction)
+            else:
+                correction = KP_FACTOR * (self.middle_value - self.right_color_sensor.get_reflected())
+                self.movement_controller.steer(correction * -1)
--- lib/movement_controller.py
+++ lib/movement_controller.py
@@ -6,20 +6,20 @@ import constants
 class MovementController:
     """Class to move the robot"""
 
-    _ROTATION_TO_DEGREE = 1
-    _DISTANCE_TO_DEGREE = 1
+    _ANGLE_TO_DEGREES = 1
+    _DISTANCE_TO_DEGREES = 1
 
     def __init__(self):
         self.moveSteering = MoveSteering(constants.LEFT_MOTOR_PORT, constants.RIGHT_MOTOR_PORT)
 
     def rotate(self, degrees, speed=100, block=True):
         """Rotate the robot a certain number of degrees. Positive is counter-clockwise"""
-        self.moveSteering.on_for_degrees(100, SpeedPercent(speed), degrees * MovementController._ROTATION_TO_DEGREE,
+        self.moveSteering.on_for_degrees(100, SpeedPercent(speed), degrees * MovementController._ANGLE_TO_DEGREES,
                                          block=block)
 
     def travel(self, distance, speed=100, block=True):
         """Make the robot move forward or backward a certain number of cm"""
-        self.moveSteering.on_for_degrees(0, speed, distance * MovementController._DISTANCE_TO_DEGREE, block=block)
+        self.moveSteering.on_for_degrees(0, speed, distance * MovementController._DISTANCE_TO_DEGREES, block=block)
 
     def steer(self, direction, speed=100):
         """Make the robot move in a direction"""

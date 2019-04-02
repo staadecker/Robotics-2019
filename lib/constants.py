5a6877fe63a24ce9ef1afb0d807d299b1d5f5d4a
Ismail-Ben <48138437+Ismail-Ben@users.noreply.github.com>
2019-04-01 23:05:38 -0400

    Added degrees constants for claw

--- lib/constants.py
+++ lib/constants.py
@@ -5,3 +5,7 @@ LEFT_MOTOR_PORT = ev3dev2.motor.OUTPUT_A
 RIGHT_MOTOR_PORT = ev3dev2.motor.OUTPUT_B
 LEFT_LINE_COLOR_SENSOR = ev3dev2.sensor.INPUT_1
 RIGHT_LINE_COLOR_SENSOR = ev3dev2.sensor.INPUT_2
+
+#Degrees predictions for claw
+deg_fibre_optic =120 
+deg_device = 90
